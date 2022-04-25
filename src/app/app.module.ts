8e151b3be6cbac5b76c4ce9513108c1211a6f84b
= <=>
2022-04-24 22:30:11 -0300

    reacomodo portfolio web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { SpinnerComponent } from '../app/components/spinner/spinner.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    SpinnerComponent,
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MatProgressSpinnerModule],
   providers: [],
   bootstrap: [AppComponent],
 })
